a77027e26d8ee04818151bffaf193a113694e975
Andrew Tribick <ajtribick@googlemail.com>
2020-02-29 14:35:39 +0100

    Add conversions to/from f64

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,26 @@
+use std::convert::From;
+
 #[derive(Debug, PartialEq, PartialOrd, Copy, Clone)]
 pub struct TwoFloat {
     hi: f64,
     lo: f64,
 }
 
-impl TwoFloat {
-    fn new(a: f64, b: f64) -> TwoFloat {
-        TwoFloat { hi: a, lo: b }
+impl From<f64> for TwoFloat {
+    fn from(value: f64) -> TwoFloat {
+        TwoFloat { hi: value, lo: 0f64 }
+    }
+}
+
+impl From<TwoFloat> for f64 {
+    fn from(value: TwoFloat) -> f64 {
+        value.hi
+    }
+}
+
+impl<'a> From<&'a TwoFloat> for f64 {
+    fn from(value: &'a TwoFloat) -> f64 {
+        value.hi
     }
 }
 
@@ -42,23 +56,15 @@ mod tests {
 
     type F64Rand<'a> = &'a mut dyn FnMut() -> f64;
 
-    randomized_test!(new_test, |rng: F64Rand| {
-        let a = rng();
-        let b = rng();
-        let value = TwoFloat::new(a, b);
-        assert_eq!(value.hi, a);
-        assert_eq!(value.lo, b);
-    });
-
     randomized_test!(copy_test, |rng: F64Rand| {
-        let a = TwoFloat::new(rng(), rng());
+        let a = TwoFloat { hi: rng(), lo: rng() };
         let b = a;
         assert_eq!(a.hi, b.hi);
         assert_eq!(a.lo, b.lo);
     });
 
     randomized_test!(clone_test, |rng: F64Rand| {
-        let a = TwoFloat::new(rng(), rng());
+        let a = TwoFloat { hi: rng(), lo: rng() };
         let b = a.clone();
         assert_eq!(a.hi, b.hi);
         assert_eq!(a.lo, b.lo);
@@ -79,14 +85,14 @@ mod tests {
     }
 
     equality_test!(equality_test, equality_ref_test, |rng: F64Rand| {
-        let a = TwoFloat::new(rng(), rng());
+        let a = TwoFloat { hi: rng(), lo: rng() };
         (a, a.clone())
     }, assert_eq);
 
     equality_test!(inequality_test, inequality_ref_test, |rng: F64Rand| {
-        let a = TwoFloat::new(rng(), rng());
+        let a = TwoFloat { hi: rng(), lo: rng() };
         (a, loop {
-            let b = TwoFloat::new(rng(), rng());
+            let b = TwoFloat { hi: rng(), lo: rng() };
             if b.hi != a.hi || b.lo != a.lo { break b; };
         })
     }, assert_ne);
@@ -94,24 +100,24 @@ mod tests {
     macro_rules! comparison_test {
         ($val_test: ident, $ref_test: ident, $op: tt, $allow_equal: expr) => {
             randomized_test!($val_test, |rng: F64Rand| {
-                let a = TwoFloat::new(rng(), rng());
+                let a = TwoFloat { hi: rng(), lo: rng() };
                 assert_eq!($allow_equal, a $op a);
         
-                let b = TwoFloat::new(a.hi, rng());
+                let b = TwoFloat { hi: a.hi, lo: rng() };
                 assert_eq!(a.lo $op b.lo, a $op b);
         
-                let c = TwoFloat::new(rng(), b.lo);
+                let c = TwoFloat { hi: rng(), lo: a.lo };
                 assert_eq!(a.hi $op c.hi, a $op c);
             });
 
             randomized_test!($ref_test, |rng: F64Rand| {
-                let a = TwoFloat::new(rng(), rng());
+                let a = TwoFloat { hi: rng(), lo: rng() };
                 assert_eq!($allow_equal, &a $op &a);
         
-                let b = TwoFloat::new(a.hi, rng());
+                let b = TwoFloat { hi: a.hi, lo: rng() };
                 assert_eq!(a.lo $op b.lo, &a $op &b);
         
-                let c = TwoFloat::new(rng(), b.lo);
+                let c = TwoFloat { hi: rng(), lo: a.lo };
                 assert_eq!(a.hi $op c.hi, &a $op &c);
             });
         };
@@ -121,4 +127,24 @@ mod tests {
     comparison_test!(greater_than_test, greater_than_ref_test, >, false);
     comparison_test!(less_equal_test, less_equal_ref_test, <=, true);
     comparison_test!(greater_equal_test, greater_equal_ref_test, >=, true);
+
+    randomized_test!(from_f64_test, |rng: F64Rand| {
+        let source = rng();
+        let result: TwoFloat = source.into();
+        assert_eq!(result.hi, source);
+        assert_eq!(result.lo, 0f64);
+    });
+
+    randomized_test!(to_f64_test, |rng: F64Rand| {
+        let source = TwoFloat { hi: rng(), lo: rng() };
+        let result: f64 = source.into();
+        assert_eq!(result, source.hi);
+    });
+
+    randomized_test!(to_f64_ref_test, |rng: F64Rand| {
+        let source = TwoFloat { hi: rng(), lo: rng() };
+        let source_ref = &source;
+        let result: f64 = source_ref.into();
+        assert_eq!(result, source.hi);
+    });
 }
