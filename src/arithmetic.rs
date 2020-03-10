827132ed19052cc6a2346aabfe03ff9845e103f0
Andrew Tribick <ajtribick@googlemail.com>
2020-03-10 19:14:01 +0100

    Update operator tests and uncomment remainder tests

--- src/arithmetic.rs
+++ src/arithmetic.rs
@@ -358,6 +358,8 @@ mod tests {
     use crate::base::*;
     use crate::test_util::*;
 
+    use rand::Rng;
+
     randomized_test!(fast_two_sum_test, |rng: F64Rand| {
         let (a, b) = get_valid_pair(rng, |a: f64, b: f64| { (a + b).is_finite() });
         let (hi, lo) = if a.abs() >= b.abs() { fast_two_sum(a, b) } else { fast_two_sum(b, a) };
@@ -435,37 +437,145 @@ mod tests {
         assert_eq!(b, b2, "Mismatch between -TwoFloat and -&TwoFloat");
     });
 
-    macro_rules! op_test_f64 {
-        ($test_name:ident, $op:tt, $op_assign:tt, $reversible:expr) => {
-            randomized_test!($test_name, |rng: F64Rand| {
-                let is_reversible = $reversible;
+    macro_rules! diff_test {
+        (%, $expected:ident, $result:ident, $lhs:ident, $rhs:ident) => ({
+            let true_difference: f64 = (($expected - $result) / $expected).into();
+            let differences: [f64; 3] = [true_difference.abs(), (1.0 - true_difference).abs(), (1.0 + true_difference).abs()];
+            let min_difference = *differences.iter().min_by(|x, y| x.partial_cmp(y).unwrap()).unwrap();
+            assert!(min_difference < 1e-10, "Out of range result of {:?} {} {:?}", $lhs, stringify!($op), $rhs);
+        });
+        ($op:tt, $expected:ident, $result:ident, $lhs:ident, $rhs:ident) => ({
+            let difference = (($expected - $result) / $expected).abs();
+            assert!(difference.abs() < 1e-10, "Out of range result of {:?} {} {:?}", $lhs, stringify!($op), $rhs);
+        });
+    }
 
+    macro_rules! op_test_f64_common {
+        ($op:tt, $op_assign:tt) => {
+            randomized_test!(ref_overloads_left_test, |rng: F64Rand| {
                 let c = rng();
-
-                let (a, b) = get_valid_pair(rng, |a: f64, b: f64| { ((a + b) $op c).is_finite() && (c $op (a + b)).is_finite() && no_overlap(a, b) });
+                let (a, b) = get_valid_pair(rng, |x, y| { ((x + y) $op c).is_finite() && no_overlap(x, y) });
                 let value = TwoFloat { hi: a, lo: b };
-
                 let result1 = value $op c;
-                let result2 = &value $op c;
-                let mut result3 = value;
-                result3 $op_assign c;
-                assert_eq!(result1, result2, "Mismatch between TwoFloat {0} f64 and &TwoFloat {0} f64", stringify!($op));
-                assert_eq!(result1, result3, "Mismatch between TwoFloat {} f64 and TwoFloat {} f64", stringify!($op), stringify!($op_assign));
+                if result1.is_valid() {
+                    let result2 = &value $op c;
+                    assert!(result2.is_valid(), "Result validity mismatch between TwoFloat {0} f64 and &TwoFloat {0} f64", stringify!($op));
+                    assert_eq!(result1, result2, "Mismatch between TwoFloat {0} f64 and &TwoFloat {0} f64", stringify!($op));
 
-                let result4 = c $op value;
-                let result5 = c $op &value;
-                if is_reversible { assert_eq!(result1, result4, "Mismatch between TwoFloat {0} f64 and f64 {0} TwoFloat", stringify!($op)); }
-                assert_eq!(result4, result5, "Mismatch between f64 {0} TwoFloat and f64 {0} &TwoFloat", stringify!($op));
-
-                let check1 = TwoFloat::from(a) $op c;
-                assert_eq_ulp!(check1.hi, a $op c, 10, "Mismatch in result of {:?} {} {:?}", a, stringify!($op), c);
-
-                if !is_reversible {
-                    let check2 = c $op TwoFloat::from(a);
-                    assert_eq_ulp!(check2.hi, c $op a, 10, "Mismatch in result of {:?} {} {:?}", c, stringify!($op), a);
-                    assert_eq_ulp!(check1.hi, a $op c, 10, "Mismatch in result of {:?} {} {:?}", c, stringify!($op), a);
+                    let mut result3 = value;
+                    result3 $op_assign c;
+                    assert!(result3.is_valid(), "Result validity mismatch between TwoFloat {} f64 and TwoFloat {} f64", stringify!($op), stringify!($op_assign));
+                    assert_eq!(result1, result3, "Mismatch between TwoFloat {} f64 and TwoFloat {} f64", stringify!($op), stringify!($op_assign));
                 }
             });
+
+            randomized_test!(ref_overloads_right_test, |rng: F64Rand| {
+                let c = rng();
+                let (a, b) = get_valid_pair(rng, |x, y| { (c $op (x + y)).is_finite() && no_overlap(x, y) });
+                let value = TwoFloat { hi: a, lo: b };
+                let result1 = c $op value;
+                if result1.is_valid() {
+                    let result2 = c $op &value;
+                    assert!(result2.is_valid(), "Result validity mismatch between f64 {0} TwoFloat and &f64 {0} &TwoFloat", stringify!($op));
+                    assert_eq!(result1, result2, "Mismatch between f64 {0} TwoFloat and &TwoFloat {0} &TwoFloat", stringify!($op));
+                }
+            });
+
+            #[test]
+            fn value_left_test() {
+                let mut rng = rand::thread_rng();
+                let high_range = rand::distributions::Uniform::new_inclusive(-1e50f64, 1e50f64);
+                let low_range = rand::distributions::Uniform::new_inclusive(-1f64, 1f64);
+
+                loop {
+                    let a = rng.sample(high_range);
+                    let (b, c) = loop {
+                        let b = rng.sample(high_range);
+                        let c = rng.sample(low_range);
+                        if no_overlap(b, c) { break (b, c); }
+                    };
+
+                    let value = TwoFloat { hi: b, lo: c };
+                    let expected = a $op b;
+                    if (!expected.is_finite()) { continue; }
+                    let result = a $op value;
+                    if (!result.is_valid()) { continue; }
+
+                    assert!(no_overlap(result.hi, result.lo), "Result of {} {} {:?} contained overlap", a, stringify!($op), value);
+
+                    diff_test!($op, expected, result, a, value);
+                    break;
+                }
+            }
+        };
+    }
+
+    macro_rules! op_test_f64 {
+        ($test_name:ident, $op:tt, $op_assign:tt, true) => {
+            #[cfg(test)]
+            mod $test_name {
+                use super::*;
+
+                op_test_f64_common!($op, $op_assign);
+
+                #[test]
+                fn reversible_test() {
+                    let mut rng = rand::thread_rng();
+                    let high_range = rand::distributions::Uniform::new_inclusive(-1e50f64, 1e50f64);
+                    let low_range = rand::distributions::Uniform::new_inclusive(-1f64, 1f64);
+                    loop {
+                        let a = rng.sample(high_range);
+                        let (b, c) = loop {
+                            let b = rng.sample(high_range);
+                            let c = rng.sample(low_range);
+                            if no_overlap(b, c) { break (b, c); }
+                        };
+
+                        let value = TwoFloat { hi: b, lo: c };
+                        let expected = a $op value;
+                        if (!expected.is_valid()) { continue; }
+
+                        let result = value $op a;
+                        assert!(no_overlap(result.hi, result.lo), "Result of {:?} {} {} contained overlap", value, stringify!($op), a);
+                        assert_eq!(result, expected, "Operation {:?} {} {} gave different result to reversed", value, stringify!($op), a);
+                        break;
+                    }
+                }
+            }
+        };
+        ($test_name:ident, $op:tt, $op_assign:tt, false) => {
+            #[cfg(test)]
+            mod $test_name {
+                use super::*;
+
+                op_test_f64_common!($op, $op_assign);
+
+                #[test]
+                fn value_right_test() {
+                    let mut rng = rand::thread_rng();
+                    let high_range = rand::distributions::Uniform::new_inclusive(-1e50f64, 1e50f64);
+                    let low_range = rand::distributions::Uniform::new_inclusive(-1f64, 1f64);
+                    loop {
+                        let a = rng.sample(high_range);
+                        let (b, c) = loop {
+                            let b = rng.sample(high_range);
+                            let c = rng.sample(low_range);
+                            if no_overlap(b, c) { break (b, c); }
+                        };
+
+                        let value = TwoFloat { hi: b, lo: c };
+                        let expected = b $op a;
+                        if (!expected.is_finite()) { continue; }
+                        let result = value $op a;
+                        if (!result.is_valid()) { continue; }
+
+                        assert!(no_overlap(result.hi, result.lo), "Result of {:?} {} {} contained overlap", value, stringify!($op), a);
+
+                        diff_test!($op, expected, result, value, a);
+                        break;
+                    }
+                }
+            }
         };
     }
 
@@ -473,33 +583,78 @@ mod tests {
     op_test_f64!(sub_f64_test, -, -=, false);
     op_test_f64!(mul_f64_test, *, *=, true);
     op_test_f64!(div_f64_test, /, /=, false);
-    //op_test_f64!(rem_f64_test, %, %=, false); // TODO write better test cases
+    op_test_f64!(rem_f64_test, %, %=, false);
 
     macro_rules! op_test {
         ($test_name:ident, $op:tt, $op_assign:tt) => {
-            randomized_test!($test_name, |rng: F64Rand| {
-                let (a, b) = get_valid_pair(rng, |a: f64, b: f64| { no_overlap(a, b) });
-                let (c, d) = get_valid_pair(rng, |c: f64, d: f64| { (a $op c).is_finite() && no_overlap(c, d) });
+            #[cfg(test)]
+            mod $test_name {
+                use super::*;
 
-                let value1 = TwoFloat { hi: a, lo: b };
-                let value2 = TwoFloat { hi: c, lo: d };
+                randomized_test!(ref_overloads_test, |rng: F64Rand| {
+                    let (a, b) = get_valid_pair(rng, |x, y| { no_overlap(x, y) });
+                    let (c, d) = get_valid_pair(rng, |x, y| { ((x + y) $op a).is_finite() && no_overlap(x, y) });
+                    let value1 = TwoFloat { hi: a, lo: b };
+                    let value2 = TwoFloat { hi: c, lo: d };
+                    let result1 = value1 $op value2;
+                    if result1.is_valid() {
+                        let result2 = &value1 $op value2;
+                        assert!(result2.is_valid(), "Result validity mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} TwoFloat", stringify!($op));
+                        assert_eq!(result1, result2, "Mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} TwoFloat", stringify!($op));
 
-                let result1 = value1 $op value2;
-                let mut result2 = value1;
-                result2 $op_assign value2;
-                assert_eq!(result1, result2, "Mismatch between TwoFloat {} TwoFloat and TwoFloat {} TwoFloat", stringify!($op), stringify!($op_assign));
+                        let result3 = value1 $op &value2;
+                        assert!(result3.is_valid(), "Result validity mismatch between TwoFloat {0} TwoFloat and TwoFloat {0} &TwoFloat", stringify!($op));
+                        assert_eq!(result1, result3, "Mismatch between TwoFloat {0} TwoFloat and TwoFloat {0} &TwoFloat", stringify!($op));
 
-                let result3 = &value1 $op value2;
-                let result4 = value1 $op &value2;
-                let result5 = &value1 $op &value2;
+                        let result4 = &value1 $op &value2;
+                        assert!(result4.is_valid(), "Result validity mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} &TwoFloat", stringify!($op));
+                        assert_eq!(result1, result4, "Mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} &TwoFloat", stringify!($op));
 
-                assert_eq!(result1, result3, "Mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} TwoFloat", stringify!($op));
-                assert_eq!(result1, result4, "Mismatch between TwoFloat {0} TwoFloat and TwoFloat {0} &TwoFloat", stringify!($op));
-                assert_eq!(result1, result5, "Mismatch between TwoFloat {0} TwoFloat and &TwoFloat {0} &TwoFloat", stringify!($op));
+                        let mut result5 = value1;
+                        result5 $op_assign value2;
+                        assert!(result5.is_valid(), "Result validity mismatch between TwoFloat {} TwoFloat and TwoFloat {} TwoFloat", stringify!($op), stringify!($op_assign));
+                        assert_eq!(result1, result5, "Mismatch between TwoFloat {} TwoFloat and TwoFloat {} TwoFloat", stringify!($op), stringify!($op_assign));
 
-                let check = TwoFloat::from(a) $op TwoFloat::from(c);
-                assert_eq_ulp!(check.hi, a $op c, 10, "Mismatch in result of {:?} {} {:?}", a, stringify!($op), c);
-            });
+                        let mut result6 = value1;
+                        result6 $op_assign &value2;
+                        assert!(result6.is_valid(), "Result validity mismatch between TwoFloat {} TwoFloat and TwoFloat {} &TwoFloat", stringify!($op), stringify!($op_assign));
+                        assert_eq!(result1, result6, "Mismatch between TwoFloat {} TwoFloat and TwoFloat {} &TwoFloat", stringify!($op), stringify!($op_assign));
+                    }
+                });
+
+                #[test]
+                fn value_test() {
+                    let mut rng = rand::thread_rng();
+                    let high_range = rand::distributions::Uniform::new_inclusive(-1e50f64, 1e50f64);
+                    let low_range = rand::distributions::Uniform::new_inclusive(-1f64, 1f64);
+
+                    loop {
+                        let (a, b) = loop {
+                            let x = rng.sample(high_range);
+                            let y = rng.sample(low_range);
+                            if no_overlap(x, y) { break (x, y); }
+                        };
+                        let (c, d) = loop {
+                            let x = rng.sample(high_range);
+                            let y = rng.sample(low_range);
+                            if no_overlap(x, y) { break (x, y); }
+                        };
+
+                        let value1 = TwoFloat { hi: a, lo: b };
+                        let value2 = TwoFloat { hi: c, lo: d };
+
+                        let expected = a $op c;
+                        if (!expected.is_finite()) { continue; }
+                        let result = value1 $op value2;
+                        if (!result.is_valid()) { continue; }
+
+                        assert!(no_overlap(result.hi, result.lo), "Result of {:?} {} {:?} contained overlap", value1, stringify!($op), value2);
+
+                        diff_test!($op, expected, result, value1, value2);
+                        break;
+                    }
+                }
+            }
         };
     }
 
@@ -507,5 +662,5 @@ mod tests {
     op_test!(sub_test, -, -=);
     op_test!(mul_test, *, *=);
     op_test!(div_test, /, /=);
-    // op_test!(rem_test, %, %=); // TODO write better test cases
+    op_test!(rem_test, %, %=);
 }
