0b917193eaa880d1e110b9049f38317c3d8749d5
Andrew Tribick <ajtribick@googlemail.com>
2021-12-08 22:34:42 +0100

    Initial setup of proc-macro crate

---
+++ twofloat_macro/src/lib.rs
@@ -0,0 +1,21 @@
+use std::str::FromStr;
+
+use proc_macro::TokenStream;
+use twofloat::TwoFloat;
+
+#[proc_macro]
+pub fn twofloat(input: TokenStream) -> TokenStream {
+    let literal = syn::parse_macro_input!(input as syn::LitStr);
+    match TwoFloat::from_str(&literal.value()) {
+        Ok(value) =>
+            format!(
+                "<::twofloat::TwoFloat as ::core::convert::TryFrom<(f64, f64)>>::try_from(({:?}f64, {:?}f64)).unwrap()",
+                value.hi(),
+                value.lo())
+            .parse()
+            .expect("Could not create token stream for TwoFloat"),
+        Err(e) => syn::Error::new(literal.span(), format!("twofloat! failed: {}", e))
+            .to_compile_error()
+            .into(),
+    }
+}
---
+++ twofloat_macro/tests/macro_tests.rs
@@ -0,0 +1,8 @@
+use hexf::hexf64;
+
+#[test]
+fn parse_test() {
+    let tf = twofloat_macro::twofloat!("0.3");
+    assert_eq!(tf.hi(), hexf64!("0x1.3333333333333p-2"));
+    assert_eq!(tf.lo(), hexf64!("0x1.999999999999ap-57"));
+}
