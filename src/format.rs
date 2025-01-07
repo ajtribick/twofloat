fa2fb52951a2409cf7c6c5270bcdfddcd60cabd6
Andrea Pelloni <apelloni90@gmail.com>
2025-01-07 10:34:51 +0100

    pretty_str as default display

--- src/format.rs
+++ src/format.rs
@@ -1,41 +1,46 @@
 use core::fmt;
+use num_bigfloat::BigFloat;
 
 use crate::TwoFloat;
 
 impl fmt::Display for TwoFloat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let sign_char = if self.lo().is_sign_positive() {
-            '+'
-        } else {
-            '-'
-        };
-        if f.sign_plus() {
-            match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:+.*} {} {:.*}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{:+} {} {}", self.hi, sign_char, libm::fabs(self.lo)),
+        write!(f, "{}", self.pretty_str())
+    }
+}
+
+impl TwoFloat {
+    // Compute number using BigFloat assuming a mantisse of size 53*2-1
+    pub fn pretty_str(&self) -> String {
+        let mut num: BigFloat = 0.0.into();
+        for float in [self.hi, self.lo] {
+            let (mut m, e) = libm::frexp(float);
+            let mut f2: BigFloat = match m.signum() {
+                1.0 => 2f64.powi(e),
+                -1.0 => -(2f64.powi(e)),
+                _ => panic!("Not Implemented"),
             }
-        } else {
-            match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:.*} {} {:.*}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{} {} {}", self.hi, sign_char, libm::fabs(self.lo)),
+            .into();
+            let mut b: f64;
+            while m != 0.0 {
+                m *= 2.0;
+                f2 /= num_bigfloat::TWO;
+                (m, b) = libm::modf(m);
+                if b.abs() == 1.0 {
+                    num += f2;
+                }
             }
         }
+        // Format String to output by reducing the significant digits to 32
+        let mut num_str = format!("{}", num);
+        if !num.is_zero() {
+            match num_str.find("e") {
+                Some(41) | None => num_str.replace_range(33..41, ""), // Positive
+                Some(42) => num_str.replace_range(34..42, ""),        // Negative
+                _ => panic!("BigFloat should have 40 significant digits"),
+            };
+        }
+        num_str
     }
 }
 
