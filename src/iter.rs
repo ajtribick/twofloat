86c761482918be404e9af3048c7e2406589e7919
Andrea Pelloni <apelloni90@gmail.com>
2025-01-08 10:43:02 +0100

    put iterator tests into module

--- src/iter.rs
+++ src/iter.rs
@@ -1,7 +1,7 @@
 use crate::TwoFloat;
-use num_traits::Zero;
 use core::iter::Sum;
 use core::ops::Add;
+use num_traits::Zero;
 
 impl<T> Sum<T> for TwoFloat
 where
@@ -16,22 +16,25 @@ where
 }
 
 #[cfg(test)]
-#[test]
-fn iter_sum_1() {
-    //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
-    let v: Vec<f64> = (1..=100).map(|x| x.into()).collect();
-    let res: TwoFloat = 5050.0.into();
-    let v_sum: TwoFloat = v.iter().sum();
+mod tests {
+    use super::*;
 
-    assert_eq!(v_sum, res);
-}
+    #[test]
+    fn iter_sum_1() {
+        //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
+        let v: Vec<f64> = (1..=100).map(|x| x.into()).collect();
+        let res: TwoFloat = 5050.0.into();
+        let v_sum: TwoFloat = v.iter().sum();
 
-#[cfg(test)]
-#[test]
-fn iter_sum_2() {
-    //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
-    let v: Vec<f64> = (1..=108).map(|x| 2f64.powi(-x)).collect();
-    let one: TwoFloat = 1.0.into();
-    let v_sum: TwoFloat = v.iter().sum();
-    assert!(v_sum - one < 1e-32);
+        assert_eq!(v_sum, res);
+    }
+
+    #[test]
+    fn iter_sum_2() {
+        //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
+        let v: Vec<f64> = (1..=108).map(|x| 2f64.powi(-x)).collect();
+        let one: TwoFloat = 1.0.into();
+        let v_sum: TwoFloat = v.iter().sum();
+        assert!(v_sum - one < 1e-32);
+    }
 }
