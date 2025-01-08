ca9b4c5ab3e19c3d5e7b8d53d200446b3eb5c5c1
Andrea Pelloni <apelloni90@gmail.com>
2025-01-08 11:09:04 +0100

    test iterator sum with vector when feature std is active

--- src/iter.rs
+++ src/iter.rs
@@ -15,27 +15,41 @@ where
     }
 }
 
-#[cfg(feature = "std")]
 #[cfg(test)]
 mod tests {
     use super::*;
 
+    #[cfg(feature = "std")]
     #[test]
-    fn iter_sum_1() {
-        //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
+    fn iter_sum_vec_1() {
         let v: Vec<f64> = (1..=100).map(|x| x.into()).collect();
-        let res: TwoFloat = 5050.0.into();
         let v_sum: TwoFloat = v.iter().sum();
+        let res: TwoFloat = 5050.0.into();
 
         assert_eq!(v_sum, res);
     }
 
+    #[cfg(feature = "std")]
+    #[test]
+    fn iter_sum_vec_2() {
+        let v: Vec<f64> = (1..=108).map(|x| 2f64.powi(-x)).collect();
+        let v_sum: TwoFloat = v.iter().sum();
+        let one: TwoFloat = 1.0.into();
+        assert!(v_sum - one < 1e-32);
+    }
+
+    #[test]
+    fn iter_sum_1() {
+        let sum : TwoFloat = (1..=100).map(|x| x as f64).sum();
+        let res: TwoFloat = 5050.0.into();
+
+        assert_eq!(sum, res);
+    }
+
     #[test]
     fn iter_sum_2() {
-        //let v: Vec<TwoFloat> = (1..=10).map(|x| Into::<TwoFloat>::into(x as f64)).collect();
-        let v: Vec<f64> = (1..=108).map(|x| 2f64.powi(-x)).collect();
+        let sum: TwoFloat = (1..=108).map(|x| 2f64.powi(-x)).sum();
         let one: TwoFloat = 1.0.into();
-        let v_sum: TwoFloat = v.iter().sum();
-        assert!(v_sum - one < 1e-32);
+        assert!(sum - one < 1e-32);
     }
 }
