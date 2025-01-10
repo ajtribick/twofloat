7dc8b6b4c491e8fcb3fa191b1a3dbdc635e852c0
Andrea Pelloni <apelloni90@gmail.com>
2025-01-10 16:44:18 +0100

    use rug for formatting the twofloat number to decimals

--- src/format.rs
+++ src/format.rs
@@ -1,96 +1,76 @@
 use core::fmt;
-use num_bigfloat::BigFloat;
+use rug::float::Round;
+use rug::ops::DivAssignRound;
+use rug::Float;
 
 use crate::TwoFloat;
 
 impl fmt::Display for TwoFloat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        match f.precision() {
-            Some(p) => {
-                if p <= 32 {
-                    write!(f, "{}", self.pretty_str(p))
-                } else {
-                    write!(f, "{}", self.pretty_str(32))
-                }
+        let num = self.to_rug_float();
+        if f.sign_plus() {
+            match f.precision() {
+                Some(p) => write!(f, "{:+.*}", p, num,),
+                None => write!(f, "{:+.32}", num),
             }
-            None => write!(f, "{}", self.pretty_str(32)),
+        } else {
+            match f.precision() {
+                Some(p) => write!(f, "{:.*}", p, num),
+                None => write!(f, "{:.32}", num),
+            }
         }
     }
 }
 
 impl TwoFloat {
-    // Compute number using BigFloat assuming a mantisse of size 53*2-1
-    fn pretty_str(&self, precision: usize) -> String {
-        assert!(precision <= 32);
-        let mut num: BigFloat = 0.0.into();
+    pub fn normalize(&mut self) {
+        let u = self.hi + self.lo;
+        let mut v = self.hi - u;
+        v += self.lo;
+
+        self.hi = u;
+        self.lo = v;
+    }
+
+    /// Compute number using BigFloat assuming a mantisse of size 53*2-1
+    fn to_rug_float(&self) -> Float {
+        let p = 106; // precision
+        let mut num: Float = Float::with_val(p, 0.0);
+
         for float in [self.hi, self.lo] {
             let (mut m, e) = libm::frexp(float);
-            let mut f2: BigFloat = match m.signum() {
-                1.0 => 2f64.powi(e),
-                -1.0 => -(2f64.powi(e)),
+            let mut f2: Float = match m.signum() {
+                1.0 => Float::with_val(p, 2f64.powi(e)),
+                -1.0 => -Float::with_val(p, 2f64.powi(e)),
                 _ => panic!("Not Implemented"),
-            }
-            .into();
+            };
             let mut b: f64;
             while m != 0.0 {
                 m *= 2.0;
-                f2 /= num_bigfloat::TWO;
+                f2.div_assign_round(2, Round::Nearest);
                 (m, b) = libm::modf(m);
                 if b.abs() == 1.0 {
-                    num += f2;
+                    num += f2.clone();
                 }
             }
         }
         // Format String to output by reducing the significant digits to 32
-        let mut num_str = format!("{}", num);
-        // Define range to remove from string
-        let range_rm = if num.is_negative() {
-            precision + 2..42
-        } else {
-            precision + 1..41
-        };
-        if !num.is_zero() {
-            match num_str.find("e") {
-                Some(41) | Some(42) | None => num_str.replace_range(range_rm, ""),
-                _ => panic!("BigFloat should have 40 significant digits"),
-            };
-        }
-        num_str
+        num
     }
 }
 
 impl fmt::LowerExp for TwoFloat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let sign_char = if self.lo().is_sign_positive() {
-            '+'
-        } else {
-            '-'
-        };
+        let num = self.to_rug_float();
         if f.sign_plus() {
             match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:+.*e} {} {:.*e}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{:+e} {} {:e}", self.hi, sign_char, libm::fabs(self.lo)),
+                Some(p) => write!(f, "{:+.*e}", p, num,),
+                None => write!(f, "{:+.32e}", num),
             }
         } else {
             match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:.*e} {} {:.*e}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{:e} {} {:e}", self.hi, sign_char, libm::fabs(self.lo)),
+                Some(p) => write!(f, "{:.*e}", p, num),
+                None => write!(f, "{:.32e}", num),
             }
         }
     }
@@ -98,36 +78,16 @@ impl fmt::LowerExp for TwoFloat {
 
 impl fmt::UpperExp for TwoFloat {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        let sign_char = if self.lo().is_sign_positive() {
-            '+'
-        } else {
-            '-'
-        };
+        let num = self.to_rug_float();
         if f.sign_plus() {
             match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:+.*E} {} {:.*E}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{:+E} {} {:E}", self.hi, sign_char, libm::fabs(self.lo)),
+                Some(p) => write!(f, "{:+.*E}", p, num,),
+                None => write!(f, "{:+.32E}", num),
             }
         } else {
             match f.precision() {
-                Some(p) => write!(
-                    f,
-                    "{:.*E} {} {:.*E}",
-                    p,
-                    self.hi,
-                    sign_char,
-                    p,
-                    libm::fabs(self.lo)
-                ),
-                None => write!(f, "{:E} {} {:E}", self.hi, sign_char, libm::fabs(self.lo)),
+                Some(p) => write!(f, "{:.*E}", p, num),
+                None => write!(f, "{:.32E}", num),
             }
         }
     }
@@ -140,33 +100,58 @@ mod test {
     #[test]
     fn display_test() {
         let value = TwoFloat { hi: 1.0, lo: 0.4 };
-        assert_eq!(format!("{:.10}", value), "1.400000000");
-        assert_eq!(format!("{:.10}", -value), "-1.400000000");
+        assert_eq!(format!("{:.29}", value), "1.4000000000000000222044604925");
+        assert_eq!(format!("{:.29}", -value), "-1.4000000000000000222044604925");
+        assert_eq!(format!("{:+.10}", value), "+1.400000000");
         assert_eq!(format!("{:.2}", value), "1.4");
         assert_eq!(format!("{:.2}", -value), "-1.4");
-        assert_eq!(format!("{:.2}", 10.0 * value), "1.4e+1");
-        assert_eq!(format!("{:.2}", -10.0 * value), "-1.4e+1");
+        assert_eq!(format!("{:+.2}", value), "+1.4");
     }
 
     #[test]
     fn lowerexp_test() {
         let value = TwoFloat { hi: 1.0, lo: -0.3 };
-        assert_eq!(format!("{:e}", value), "1e0 - 3e-1");
-        assert_eq!(format!("{:e}", -value), "-1e0 + 3e-1");
-        assert_eq!(format!("{:+e}", value), "+1e0 - 3e-1");
-        assert_eq!(format!("{:.2e}", value), "1.00e0 - 3.00e-1");
-        assert_eq!(format!("{:.2e}", -value), "-1.00e0 + 3.00e-1");
-        assert_eq!(format!("{:+.2e}", value), "+1.00e0 - 3.00e-1");
+        assert_eq!(
+            format!("{:e}", value),
+            "7.0000000000000001110223024625157e-1"
+        );
+        assert_eq!(
+            format!("{:e}", -value),
+            "-7.0000000000000001110223024625157e-1"
+        );
+        assert_eq!(
+            format!("{:+e}", value),
+            "+7.0000000000000001110223024625157e-1"
+        );
+        assert_eq!(format!("{:.2e}", value), "7.0e-1");
+        assert_eq!(format!("{:.2e}", -value), "-7.0e-1");
+        assert_eq!(format!("{:+.2e}", value), "+7.0e-1");
     }
 
     #[test]
     fn upperexp_test() {
         let value = TwoFloat { hi: 1.0, lo: 0.3 };
-        assert_eq!(format!("{:E}", value), "1E0 + 3E-1");
-        assert_eq!(format!("{:E}", -value), "-1E0 - 3E-1");
-        assert_eq!(format!("{:+E}", value), "+1E0 + 3E-1");
-        assert_eq!(format!("{:.2E}", value), "1.00E0 + 3.00E-1");
-        assert_eq!(format!("{:.2E}", -value), "-1.00E0 - 3.00E-1");
-        assert_eq!(format!("{:+.2E}", value), "+1.00E0 + 3.00E-1");
+        assert_eq!(
+            format!("{:E}", value),
+            "1.2999999999999999888977697537484E0"
+        );
+        assert_eq!(
+            format!("{:E}", -value),
+            "-1.2999999999999999888977697537484E0"
+        );
+        assert_eq!(
+            format!("{:+E}", value),
+            "+1.2999999999999999888977697537484E0"
+        );
+        assert_eq!(format!("{:.2E}", value), "1.3E0");
+        assert_eq!(format!("{:.2E}", -value), "-1.3E0");
+        assert_eq!(format!("{:+.2E}", value), "+1.3E0");
+    }
+
+    #[test]
+    fn precision_test() {
+        // Check representation of TwoFloat with a mantissa representing 2^-200
+        let a = TwoFloat::new_add(2f64.powi(-200), 0.0);
+        assert_eq!(format!("{}", a), "6.2230152778611417071440640537801e-61");
     }
 }
