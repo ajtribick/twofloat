6bc39c758b758ca4b34058933f76441ca524db85
whipstein <matt_braunstein@yahoo.com>
2026-03-01 07:28:26 -0600

    Added Product trait

--- src/iter.rs
+++ src/iter.rs
@@ -1,7 +1,7 @@
 use crate::TwoFloat;
-use core::iter::Sum;
-use core::ops::Add;
-use num_traits::Zero;
+use core::iter::{Product, Sum};
+use core::ops::{Add, Mul};
+use num_traits::{One, Zero};
 
 impl<T> Sum<T> for TwoFloat
 where
@@ -12,6 +12,18 @@ where
         I: Iterator<Item = T>,
     {
         iter.fold(Self::zero(), <Self>::add)
+    }
+}
+
+impl<T> Product<T> for TwoFloat
+where
+    Self: Mul<T, Output = Self>,
+{
+    fn product<I>(iter: I) -> Self
+    where
+        I: Iterator<Item = T>,
+    {
+        iter.fold(Self::one(), <Self>::mul)
     }
 }
 
@@ -38,6 +50,25 @@ mod tests {
         assert!(v_sum - one < 1e-32);
     }
 
+    #[cfg(feature = "std")]
+    #[test]
+    fn iter_product_vec_1() {
+        let v: Vec<f64> = (1..=10).map(|x| x.into()).collect();
+        let v_product: TwoFloat = v.iter().product();
+        let res: TwoFloat = 3628800.0.into();
+
+        assert_eq!(v_product, res);
+    }
+
+    #[cfg(feature = "std")]
+    #[test]
+    fn iter_product_vec_2() {
+        let v: Vec<f64> = (1..=18).map(|x| 2f64.powi(-x)).collect();
+        let v_product: TwoFloat = v.iter().product();
+        let one: TwoFloat = 1.0.into();
+        assert!(v_product - one < 1e-32);
+    }
+
     #[test]
     fn iter_sum_1() {
         let sum: TwoFloat = (1..=100).map(|x| x as f64).sum();
@@ -51,5 +82,20 @@ mod tests {
         let sum: TwoFloat = (1..=108).map(|x| 2f64.powi(-x)).sum();
         let one: TwoFloat = 1.0.into();
         assert!(sum - one < 1e-32);
+    }
+
+    #[test]
+    fn iter_product_1() {
+        let product: TwoFloat = (1..=10).map(|x| x as f64).product();
+        let res: TwoFloat = 3628800.0.into();
+
+        assert_eq!(product, res);
+    }
+
+    #[test]
+    fn iter_product_2() {
+        let product: TwoFloat = (1..=18).map(|x| 2f64.powi(-x)).product();
+        let one: TwoFloat = 1.0.into();
+        assert!(product - one < 1e-32);
     }
 }
